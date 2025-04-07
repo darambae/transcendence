778a6a8133edc512b3bb87e33d406f145bcfd992
Daram Bae <baedaram@gmail.com>
2025-04-07 19:27:31 +0000

    Django & Postgresql & nginx containers are set up

--- django/app/settings.py
+++ django/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
---
+++ django/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+if ! python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | grep "True"; then
+  echo "Creating superuser 'admin'..."
+  python manage.py createsuperuser --username ${DJANGO_SUPERUSER} --email ${DJANGO_SUPERUSER_EMAIL} --noinput
+else
+  echo "A superuser already exists."
+fi
+
+#python manage.py collectstatic --noinput
+exec "$@"
