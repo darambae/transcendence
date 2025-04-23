ae6558608e21728f9c5b95008a8e29eb34b17b45
Daram Bae <baedaram@gmail.com>
2025-04-23 13:54:50 +0000

    Saving before making a new branch(elk)

--- backend/user_service/entrypoint.sh
+++ backend/user_service/entrypoint.sh
@@ -10,4 +10,6 @@ else
 fi
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 exec "$@"
---
+++ backend/user_service/user/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from . import views
+
+urlpatterns = [
+    path('', views.index_view.as_view(), name='index'),
+    path('admin/', admin.site.urls),
+    path('login/', views.login_view.as_view(), name='login'),
+    path('register/', views.register_view.as_view(), name='register'),
+    path('profile/', views.profile_view.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change_password/', PasswordChangeView.as_view(), name='change_password'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done')
+    # Add your app URLs here
+    # /
+    # profile/
+    # login/
+    # subscribe/
+    # ai
+    # duel
+
+]
