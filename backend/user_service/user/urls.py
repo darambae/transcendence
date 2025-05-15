df9038997b31c2008d89c39687359373580ba80e
darambae <64197421+jazzy1101@users.noreply.github.com>
2025-05-15 14:27:22 +0200

    Frontend and Django app and docker are connected

--- backend/user_service/user/urls.py
+++ backend/user_service/user/urls.py
@@ -16,24 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
-from . import views
+# from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+# from . import views
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.index_view.as_view(), name='index'),
     path('admin/', admin.site.urls),
-    path('login/', views.login_view.as_view(), name='login'),
-    path('register/', views.register_view.as_view(), name='register'),
-    path('profile/', views.profile_view.as_view(), name='profile'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('change_password/', PasswordChangeView.as_view(), name='change_password'),
-    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done')
-    # Add your app URLs here
-    # /
-    # profile/
-    # login/
-    # subscribe/
-    # ai
-    # duel
-
+	# path('user_service/', include('user.urls')),
+	path('', TemplateView.as_view(template_name="index.html"))
 ]
+
+# urlpatterns = [
+#     path('', views.index_view.as_view(), name='index'),
+#     path('admin/', admin.site.urls),
+#     path('login/', views.login_view.as_view(), name='login'),
+#     path('register/', views.register_view.as_view(), name='register'),
+#     path('profile/', views.profile_view.as_view(), name='profile'),
+#     path('logout/', LogoutView.as_view(), name='logout'),
+#     path('change_password/', PasswordChangeView.as_view(), name='change_password'),
+#     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done')
+#     # Add your app URLs here
+# 
+# ]
