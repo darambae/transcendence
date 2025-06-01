67415c5177c358b78c473129c874f4689146318b
darambae <64197421+jazzy1101@users.noreply.github.com>
2025-06-01 16:15:54 +0200

    API form is added to Readme file & Convention file is added to doc directory & Merged with elk branch and commented out elk part to detach from other services

--- backend/server_pong/entrypoint.sh
+++ backend/server_pong/entrypoint.sh
@@ -3,15 +3,15 @@ set -ex
 
 #Check if ELK is up and running
 #Wait for Elasticsearch to be ready
-until curl -s -k -u "elastic:${ELASTIC_PASSWORD}" "https://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s" | grep -q '"status":"yellow"\|"status":"green"'; do
-  echo "Waiting for Elasticsearch to be ready..."
-  sleep 10
-done
+# until curl -s -k -u "elastic:${ELASTIC_PASSWORD}" "https://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s" | grep -q '"status":"yellow"\|"status":"green"'; do
+#   echo "Waiting for Elasticsearch to be ready..."
+#   sleep 10
+# done
 
-# Wait for Logstash to be ready (adjust port if needed)
-until curl -sf "http://logstash:9600/_node/pipelines?pretty"; do
-  echo "Waiting for Logstash to be ready..."
-  sleep 15
-done
+# # Wait for Logstash to be ready (adjust port if needed)
+# until curl -sf "http://logstash:9600/_node/pipelines?pretty"; do
+#   echo "Waiting for Logstash to be ready..."
+#   sleep 15
+# done
 
 exec "$@"
