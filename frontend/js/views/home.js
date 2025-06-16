846df4d9ed90ec44062b797d884ee576144143a0
mamar66 <omarfelk66@gmail.com>
2025-06-16 16:46:02 +0200

    add seting end avatar in bar tach

--- frontend/js/views/home.js
+++ frontend/js/views/home.js
@@ -1,7 +1,6 @@
 import { getCookie } from "../utils.js";
 
 export function homeController() {
-	console.log("here in home function");
 
 	// const csrf = getCookie('csrftoken');
 	// console.log("csrf home: ", csrf);
--- frontend/js/views/user.js
+++ frontend/js/views/user.js
@@ -2,6 +2,26 @@ export function userController() {
 	const dropdownBtn = document.getElementById('avatarDropdownBtn');
 	const dropdownMenu = document.getElementById('customDropdownMenu');
 
+	
+	const token = sessionStorage.getItem("accessToken");
+
+	fetch("https://localhost:8443/user-service/avatar/", {
+		method: "GET",
+		headers: {
+		  "Authorization": `Bearer ${token}`,
+		}
+	  })
+	  .then(res => {
+		if (!res.ok) throw new Error("Erreur lors de la récupération de l'avatar");
+		return res.blob();
+	  })
+	  .then(blob => {
+		const imgUrl = URL.createObjectURL(blob);
+		document.getElementById("user-avatar").src = imgUrl;
+	  })
+	  .catch(err => {
+		console.error("Erreur:", err);
+	  });
 	if (dropdownBtn && dropdownMenu) {
 		dropdownBtn.addEventListener('click', () => {
 			dropdownMenu.classList.toggle('show');
