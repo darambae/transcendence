c5f6b5ed6b38047a5478872639ea6f4ef6eb22d2
mamar66 <omarfelk66@gmail.com>
2025-06-20 12:43:07 +0200

    start friends modif card_profile end add model tab friend in access

--- backend/access_postgresql/api/models.py
+++ backend/access_postgresql/api/models.py
@@ -25,6 +25,7 @@ class USER(AbstractBaseUser, PermissionsMixin):
 	def toJson(self):
 		return {"user_id" : self.id, "username" : self.user_name, "avatar" : self.avatar, "invites" : []}
 
+
 class MATCHTABLE(models.Model):
     matchKey = models.CharField(unique=True, max_length=100)
     dateMatch = models.DateTimeField(auto_now_add=True)
@@ -35,3 +36,28 @@ class MATCHTABLE(models.Model):
 
     def __str__(self):
         return f"{self.username1} {self.score1} - {self.score2} {self.username2}"
+
+
+class FRIEND(models.Model):
+    from_user = models.ForeignKey(
+        USER, on_delete=models.CASCADE, related_name='sent_requests'
+    )
+    to_user = models.ForeignKey(
+        USER, on_delete=models.CASCADE, related_name='received_requests'
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ('pending', 'Pending'),
+            ('accepted', 'Accepted'),
+            ('rejected', 'Rejected'),
+        ],
+        default='pending'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')
+
+    def __str__(self):
+        return f"{self.from_user} ➜ {self.to_user} ({self.status})"
