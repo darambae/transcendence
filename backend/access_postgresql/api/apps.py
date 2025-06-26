59f0c93afb918b65628cf43e18e6d55dea66ae96
mamar66 <omarfelk66@gmail.com>
2025-06-26 14:17:29 +0200

    add dashbord hystorique matchh

--- backend/access_postgresql/api/apps.py
+++ backend/access_postgresql/api/apps.py
@@ -1,9 +1,6 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
