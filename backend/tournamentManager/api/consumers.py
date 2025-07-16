7538350876485ec004353df55a3f0cdc7732d09f
Rafael Michel--Benoit <rmichel-@c3r5p2.42perpignan.fr>
2025-07-16 23:11:50 +0200

    I gave up 2-1-1, but everything else is working

--- backend/tournamentManager/api/consumers.py
+++ backend/tournamentManager/api/consumers.py
@@ -93,13 +93,14 @@ async def setResults(trnmt, username, roundMatch, mKey) :
 	else :
 		# print("---------------------------------! Final match !---------------------------------")
 		if trnmt.matchWinnerBracket.key == mKey :
-			# print("Soooo -> ", trnmt.matchLoserBracket.launchable, file=sys.stderr)
+			print("Soooo1 -> ", trnmt.matchLoserBracket.launchable, file=sys.stderr)
 			if not trnmt.matchLoserBracket.launchable :
 				nextMatch = "final-rounds"
 			else :
 				nextMatch = "set-results"
 			trnmt.matchWinnerBracket.played = True
 			trnmt.matchLoserBracket.launchable = True
+			print("Soooo2 -> ", trnmt.matchLoserBracket.launchable, file=sys.stderr)
 			if username == trnmt.matchWinnerBracket.p1.username :
 				trnmt.first = trnmt.matchWinnerBracket.p1
 				trnmt.second = trnmt.matchWinnerBracket.p2
@@ -224,7 +225,7 @@ class GameConsumer(AsyncWebsocketConsumer):
 				await self.launchGame(trnmtDict[self.room_group_name].match2, 1)
 		
 		elif action == "final-matches" :
-			trnmtDict[self.room_group_name].matchLoserBracket.initValues(trnmtDict[self.room_group_name].matchWinnerBracket)
+			# trnmtDict[self.rsoom_group_name].matchLoserBracket.initValues(trnmtDict[self.room_group_name].matchWinnerBracket)
 			print(f"finals matches consumer |\n trnmtDict[self.room_group_name].matchLoserBracket.launchable : {trnmtDict[self.room_group_name].matchLoserBracket.launchable} ||\n trnmtDict[self.room_group_name].matchWinnerBracket.launchable {trnmtDict[self.room_group_name].matchWinnerBracket.launchable} ||\n trnmtDict[self.room_group_name].matchWinnerBracket.played : {trnmtDict[self.room_group_name].matchWinnerBracket.played} ||\n trnmtDict[self.room_group_name].matchLoserBracket.played {trnmtDict[self.room_group_name].matchLoserBracket.played} ", file=sys.stderr)
 			# if self.name == trnmtDict[self.room_group_name].matchWinnerBracket.p1.username:
 			await self.send(text_data=json.dumps({"t_state" : "final-match-preview", "tkey" : self.room_group_name, "matchWinner" : {"player1" : trnmtDict[self.room_group_name].matchWinnerBracket.p1.username, "player2" : trnmtDict[self.room_group_name].matchWinnerBracket.p2.username}, "matchLooser" : {"player1" : trnmtDict[self.room_group_name].matchLoserBracket.p1.username, "player2" : trnmtDict[self.room_group_name].matchLoserBracket.p2.username}}))
